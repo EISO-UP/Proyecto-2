9f6601de0a4a69480337011959d6ec22f192ca41
Enrique <0208738@up.edu.mx>
2022-10-29 13:03:45 -0500

    Ultima actualizacion

--- models/database.js
+++ models/database.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema;
 const DCollection = new Schema(
   {
     money: { rating: Number },
-    collectables: { type: [String], required: true },
+    collectables: [{
+      collection_name: {type: String, require: true},
+      amount: {rating: Number},
+      collection_price: {rating: Number}
+    }],
     operation: {type: String, require: true},
-    collection_name: {type: String, require: true},
-    amount: {rating: Number},
-    collection_price: {rating: Number}
+    
   },
   { timestamps: true }
 );
